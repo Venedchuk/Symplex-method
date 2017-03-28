c2f495175bbbeed5f88543b3d2f2eb7df08a3010
METEOR\andrii <sneyp67@gmail.com>
2017-03-28 20:28:41 +0300

    design table

--- Symplex-method/MainWindow.xaml.cs
+++ Symplex-method/MainWindow.xaml.cs
@@ -20,13 +20,14 @@ namespace Symplex_method
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {        
+    {
         public MainWindow()
         {
             InitializeComponent();
         }
 
         public List<List<TextBox>> SymplexTable = new List<List<TextBox>>();
+
 
         #region Validation
 
@@ -75,23 +76,30 @@ namespace Symplex_method
             var rnd = new Random();
 #endif
 
+
+            List<TextBox> ListBoxValue = new List<TextBox>();
+            List<TextBox> ListBoxBasis = new List<TextBox>();
             var varCount = 0; // columns
             int.TryParse(variablesCount.Text, out varCount);
 
             var limCount = 0;  //rows
             int.TryParse(limitsCount.Text, out limCount);
 
+            designTable(varCount, limCount);
+
             SymplexTable = new List<List<TextBox>>();
             listBox.Items.Clear();
-
+            
             for (int x = 0; x <= limCount; x++)
             {
+
+
                 var rowModel = new List<TextBox>();
                 var grid = new Grid();
                 for (int y = 0; y <= varCount; y++)
                 {
                     var box = new TextBox();
-                    box.Margin = new Thickness(35*y,0,-35*y,0);
+                    box.Margin = new Thickness(35 * y, 0, -35 * y, 0);
                     box.Width = 30;
                     box.TextChanged += ValidateInputBoth;
 
@@ -106,6 +114,55 @@ namespace Symplex_method
                 listBox.Items.Add(grid);
             }
 
+
+        }
+
+        private void designTable(int varCount = 0, int limCount = 0)
+        {
+            #region horiz_table
+            listBoxValue.Items.Clear();
+
+            var gridForView = new Grid();
+            int i;
+            for (i = 0; i < varCount; i++)
+            {
+                var box = new TextBox();
+                box.Margin = new Thickness(35 * i, 0, -35 * i, 0);
+                box.Width = 30;
+                box.Text = "x" + ++i;
+                i--;
+
+                gridForView.Children.Add(box);
+
+            }
+
+            var box1 = new TextBox();
+            box1.Margin = new Thickness(35 * i, 0, -35 * i, 0);
+            box1.Width = 30;
+            box1.Text = "free";
+            gridForView.Children.Add(box1);
+
+            listBoxValue.Items.Add(gridForView);//ramka horizontal
+
+            #endregion
+            #region vert_table
+            var rowModel = new List<TextBox>();
+            var grid = new Grid();
+            listBoxBasis.Items.Clear();
+            gridForView = new Grid();
+            for (i = 0; i < limCount; i++)
+            {
+                var box = new TextBox();
+                box.Width = 30;
+                box.Text = "x" + ++i;
+                i--;
+                listBoxBasis.Items.Add(box);
+            }
+            var box2 = new TextBox();
+            box2.Width = 30;
+            box2.Text = "Func";
+            listBoxBasis.Items.Add(box2);//vertical
+            #endregion
 
         }
 
